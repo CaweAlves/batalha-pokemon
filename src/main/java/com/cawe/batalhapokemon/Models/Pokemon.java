7f16fca2252cf4ddcbc9c83329c88b49da540252
Cawe Alves <cawe.wewe04@gmail.com>
2023-08-28 18:00:59 -0300

    feat: create pokemon model

---
+++ src/main/java/com/cawe/batalhapokemon/Models/Pokemon.java
@@ -0,0 +1,25 @@
+package com.cawe.batalhapokemon.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "pokemons")
+public class Pokemon {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String tipo;
+    @Column(nullable = false)
+    private Integer pp;
+}
