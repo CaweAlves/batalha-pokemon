36df1da8ec2c768755efe0aaa367993014cd6f95
Cawe Alves <cawe.wewe04@gmail.com>
2023-08-28 14:06:18 -0300

    init

---
+++ src/main/java/com/cawe/batalhapokemon/BatalhaPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.cawe.batalhapokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatalhaPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatalhaPokemonApplication.class, args);
+	}
+
+}
