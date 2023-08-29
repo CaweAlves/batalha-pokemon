d029f44a8017417a44f5348dcfee155cb4aa4ddb
Cawe541 <cawe.wewe04@gmail.com>
2023-08-28 21:16:47 -0300

    feat: create battle model

---
+++ src/main/java/com/cawe/batalhapokemon/Models/Batalha.java
@@ -0,0 +1,22 @@
+package com.cawe.batalhapokemon.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "batalhas")
+public class Batalha {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private LocalDateTime data;
+}
