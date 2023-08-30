a4515b0c128e752bc0dc9d898a9cdc06b46b90d6
Cawe Alves <cawe.wewe04@gmail.com>
2023-08-30 13:57:37 -0300

    wip: trainer crud

--- src/main/java/com/cawe/batalhapokemon/Controllers/TreinadorController.java
+++ src/main/java/com/cawe/batalhapokemon/Controllers/TreinadorController.java
@@ -4,6 +4,7 @@ import com.cawe.batalhapokemon.DTOs.TreinadorDto;
 import com.cawe.batalhapokemon.Models.Treinador;
 import com.cawe.batalhapokemon.Services.TreinadorService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,5 +27,29 @@ public class TreinadorController {
         Treinador treinadorResponse = treinadorService.criarTreinador(treinador);
 
         return treinadorResponse;
+    }
+
+    @RequestMapping(path = "/treinador", method = RequestMethod.PUT)
+    @ResponseBody
+    private Treinador atualizarTreinador(@RequestBody @Validated TreinadorDto treinadorDto) throws Exception {
+        var treinador = new Treinador();
+
+        BeanUtils.copyProperties(treinadorDto, treinador);
+
+        Treinador treinadorResponse = treinadorService.atualizarTreinador(treinador);
+
+        return treinadorResponse;
+    }
+
+    @RequestMapping(path = "/treinador", method = RequestMethod.DELETE)
+    @ResponseBody
+    private HttpStatus deletarTreinador(@RequestBody @Validated TreinadorDto treinadorDto) throws Exception {
+        var treinador = new Treinador();
+
+        BeanUtils.copyProperties(treinadorDto, treinador);
+
+        treinadorService.deletarTreinador(treinador);
+
+        return HttpStatus.OK;
     }
 }
--- src/main/java/com/cawe/batalhapokemon/Services/TreinadorService.java
+++ src/main/java/com/cawe/batalhapokemon/Services/TreinadorService.java
@@ -4,6 +4,8 @@ import com.cawe.batalhapokemon.Models.Treinador;
 import com.cawe.batalhapokemon.Repositories.TreinadorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TreinadorService {
 
@@ -13,13 +15,17 @@ public class TreinadorService {
         this.treinadorRepository = treinadorRepository;
     }
 
+    public Optional<Treinador> buscarTreinador(Treinador treinador){
+        return treinadorRepository.findById(treinador.getId());
+    }
+
     public Treinador criarTreinador(Treinador treinador) {
         return treinadorRepository.save(treinador);
     }
 
     public Treinador atualizarTreinador(Treinador treinador) throws Exception {
         if (treinador.getId() == null) {
-            throw new Exception("para atualizar um funcionario é necessario enviar o id");
+            throw new Exception("para atualizar um treinador é necessario enviar o id");
         }
 
         return treinadorRepository.save(treinador);
@@ -27,7 +33,7 @@ public class TreinadorService {
 
     public void deletarTreinador(Treinador treinador) throws Exception {
         if (treinador.getId() == null) {
-            throw new Exception("para atualizar um funcionario é necessario enviar o id");
+            throw new Exception("para deletar um treinador é necessario enviar o id");
         }
 
         treinadorRepository.delete(treinador);
